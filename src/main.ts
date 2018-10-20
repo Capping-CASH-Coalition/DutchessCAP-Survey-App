810c9e8bd4b626ea8a3f957b75327ba5b9b2400f
Brian Henderson <brian.henderson1@marist.edu>
2018-10-20 18:44:11 -0400

    revert to 9fd5f2985288f26d202e09f01f99cd2ab288211f

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { Chart } from '../node_modules/chart.js/dist/Chart.bundle.js';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
 
 if (environment.production) {
   enableProdMode();

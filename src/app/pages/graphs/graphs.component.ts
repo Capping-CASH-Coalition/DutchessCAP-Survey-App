df0327d5109627a90ce0520d05b53318200bdab6
Brian Henderson <brian.henderson1@marist.edu>
2018-10-21 16:23:26 -0400

    Added filter panel

--- src/app/pages/graphs/graphs.component.ts
+++ src/app/pages/graphs/graphs.component.ts
@@ -1,47 +1,83 @@
-import { Component, AfterViewInit } from '@angular/core';
-//import { Chart } from "../../../../node_modules/@types/chart.js";
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { Chart } from "chart.js";
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
     selector: 'app-graphs',
     templateUrl: './graphs.component.html',
+    styleUrls: ['./graphs.component.css']
 })
-export class GraphsComponent implements AfterViewInit {
+export class GraphsComponent implements AfterViewInit, OnInit {
 
-   title = "Graphs";
-   subtitle = "Gonna make some nice graphs";
-   count = 0;
-
-   addCount() {
-      this.count++;
-   }
-
+   myForm: FormGroup;
    canvas: any;
    ctx: any;
+   graphChart: Chart;
 
-   ngAfterViewInit() {
-      this.canvas = document.getElementById('graphCanvas');
-      this.ctx = this.canvas.getContext('2d');
-      let myChart = new Chart(this.ctx, {
-        type: 'pie',
-        data: {
-            labels: ["New", "In Progress", "On Hold"],
-            datasets: [{
-                label: '# of Votes',
-                data: [1,2,3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-          responsive: false
-        }
-      });
+   graphTypes: Array<string> = ['pie', 'bar', 'doughnut', 'polarArea'];
+
+   graphDataFilters: Array<string> = ['age', 'language', 'gender', 'location', 'experience'];
+   
+   currentGraphType: string = 'doughnut';
+
+   chartData = {
+      labels: ["A", "B", "C", "D"],
+      datasets: [{
+          label: '# of Votes',
+          data: [1,2,3,4],
+          backgroundColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(54, 120, 235, 1)'
+          ],
+          borderWidth: 1
+      }]
+   }
+
+   chartOptions = {
+      responsive: false
     }
+
+   private generateGraph(): void {
+      this.graphChart = new Chart(this.ctx, {
+         type: this.currentGraphType,
+         data: this.chartData,
+         options: this.chartOptions
+       });
+    }
+
+    private onChanges(): void {
+      this.myForm.get('graphType').valueChanges.subscribe(val => {
+        this.currentGraphType = val;
+      });
+      
+      this.myForm.get('filterTopLevel').valueChanges.subscribe(val => {
+         this.currentGraphType = val;
+       });
+       
+       this.myForm.get('filterSubLevel').valueChanges.subscribe(val => {
+         this.currentGraphType = val;
+       });
+    }
+
+
+   ngOnInit() {
+      this.myForm = new FormGroup({
+         graphType : new FormControl(this.graphTypes[0]),
+         filterTopLevel : new FormControl(this.graphDataFilters[0]),
+         filterSubLevel : new FormControl(this.graphDataFilters[1])
+      });
+      this.onChanges();
+      
+   }
+
+    ngAfterViewInit() {
+      this.canvas = document.getElementById('graphCanvas');
+      this.ctx = this.canvas.getContext('2d');   
+      this.generateGraph();
+    } 
    
     
 }
--- src/app/pages/graphs/graphs.module.ts
+++ src/app/pages/graphs/graphs.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import { Chart } from "../../../../node_modules/@types/chart.js";
+//import {FormsModule} from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import {GraphsComponent} from "./graphs.component";
 import {graphsRoutes} from "./graphs.routes";
@@ -10,7 +10,8 @@ import {graphsRoutes} from "./graphs.routes";
 @NgModule({
   imports: [
       CommonModule,
-      FormsModule,
+      //FormsModule,
+      ReactiveFormsModule,
       RouterModule.forChild(graphsRoutes)
   ],
   declarations: [

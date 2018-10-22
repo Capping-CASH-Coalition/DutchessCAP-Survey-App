75ef70f975f213ff7ddabd55bb49b6ce3d67ed44
Brian Henderson <brian.henderson1@marist.edu>
2018-10-21 20:47:51 -0400

    A lot going on here, going to try a different method. commiting for reference

--- src/app/pages/graphs/graphs.component.ts
+++ src/app/pages/graphs/graphs.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { Chart } from "chart.js";
 import { FormGroup, FormControl } from '@angular/forms';
+import { GraphData } from './graphData.component';
 
 
 @Component({
@@ -16,16 +17,21 @@ export class GraphsComponent implements AfterViewInit, OnInit {
    graphChart: Chart;
 
    graphTypes: Array<string> = ['pie', 'bar', 'doughnut', 'polarArea'];
-
    graphDataFilters: Array<string> = ['age', 'language', 'gender', 'location', 'experience'];
    
-   currentGraphType: string = 'doughnut';
+   currentGraphData: GraphData;
+   currentGraphType: string = 'polarArea';
+      
+   currentTopLevelFilter: string;
+   currentSubLevelFilter: string;
+
+   graphData: Array<GraphData>;
 
    chartData = {
       labels: ["A", "B", "C", "D"],
       datasets: [{
           label: '# of Votes',
-          data: [1,2,3,4],
+          data: [1.2,2.5,3.1, 2.9],
           backgroundColor: [
               'rgba(255, 99, 132, 1)',
               'rgba(54, 162, 235, 1)',
@@ -37,7 +43,7 @@ export class GraphsComponent implements AfterViewInit, OnInit {
    }
 
    chartOptions = {
-      responsive: false
+      responsive: false,
     }
 
    private generateGraph(): void {
@@ -45,22 +51,75 @@ export class GraphsComponent implements AfterViewInit, OnInit {
          type: this.currentGraphType,
          data: this.chartData,
          options: this.chartOptions
-       });
+      });
+
+      
     }
+
+   private updateGraphData(key:string): void {
+      this.currentGraphData = this.getGraphDataFromKey(key);
+      this.removeData(this.graphChart);
+      this.addData(this.graphChart, this.currentGraphData.labels, this.currentGraphData.data);
+   }
+
+   private removeData(chart: Chart): void {
+      chart.data.labels.pop();
+      chart.data.datasets.forEach((dataset) => {
+          dataset.data.pop();
+      });
+      chart.update();
+   }
+
+   private addData(chart, label, data) {)
+      chart.data.labels.push(label);
+      chart.data.datasets.forEach((dataset) => {
+         dataset.data.push(data);
+      });
+      chart.update();
+   }
+
+   private getGraphDataFromKey(key: string): GraphData {
+      for (let i = 0; i < this.graphData.length; i++) {
+         if (this.graphData[i].key === key) {
+            return this.graphData[i];
+         }
+      }
+   }
+
+   private addToDataArray(key:string, labels:Array<string>, data:Array<number>): void {
+      let gd = new GraphData(key, labels, data);
+      this.graphData.push(gd);
+   }
+
 
     private onChanges(): void {
       this.myForm.get('graphType').valueChanges.subscribe(val => {
         this.currentGraphType = val;
+        console.log("Graph Type changed to: " + this.currentGraphType);
       });
       
       this.myForm.get('filterTopLevel').valueChanges.subscribe(val => {
-         this.currentGraphType = val;
+         this.currentTopLevelFilter = val;
+         console.log("Top Level changed to: " + this.currentTopLevelFilter);
        });
        
        this.myForm.get('filterSubLevel').valueChanges.subscribe(val => {
-         this.currentGraphType = val;
+         this.currentSubLevelFilter = val;
+         console.log("Sub Level changed to: " + this.currentSubLevelFilter);
        });
     }
+
+
+
+    updateTopFilter(filter: string) {
+       this.currentTopLevelFilter = filter;
+       this.updateGraphData();
+    }
+
+    updateSubFilter(filter: string) {
+      this.currentSubLevelFilter = filter;
+   }
+
 
 
    ngOnInit() {
@@ -69,15 +128,21 @@ export class GraphsComponent implements AfterViewInit, OnInit {
          filterTopLevel : new FormControl(this.graphDataFilters[0]),
          filterSubLevel : new FormControl(this.graphDataFilters[1])
       });
+
+      this.addToDataArray("language", ["english", "spanish", "chineese"],[59, 62, 12]);
+      this.addToDataArray( "age", ["18-21", "22-29", "30-39","40-49","50-59","60+"], [18, 22, 48, 21, 30, 4]);
+      this.addToDataArray( "gender", ["male", "female"], [37, 49]);
+
       this.onChanges();
-      
    }
 
     ngAfterViewInit() {
       this.canvas = document.getElementById('graphCanvas');
       this.ctx = this.canvas.getContext('2d');   
       this.generateGraph();
+      this.onChanges();
     } 
    
+   // this.addToDataArray("language", ["english", "spanish", "chineese"],[59, 62, 12]);
     
 }
--- src/app/pages/graphs/graphs.module.ts
+++ src/app/pages/graphs/graphs.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from "@angular/router";
-//import {FormsModule} from "@angular/forms";
+import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from '@angular/forms';
 
 import {GraphsComponent} from "./graphs.component";
@@ -10,7 +9,6 @@ import {graphsRoutes} from "./graphs.routes";
 @NgModule({
   imports: [
       CommonModule,
-      //FormsModule,
       ReactiveFormsModule,
       RouterModule.forChild(graphsRoutes)
   ],

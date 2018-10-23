284753364237aee5b2ce6b54ba1425f82bc7dccf
Brian Henderson <brian.henderson1@marist.edu>
2018-10-23 09:21:44 -0400

    Added line and area charts

--- src/app/pages/graphs/graphData.component.ts
+++ src/app/pages/graphs/graphData.component.ts
@@ -2,5 +2,5 @@ export class GraphData {
    constructor( public key: string,
                 public labels: Array<string>,
                 public data: Array<number>
-               ){};
+   ){};
 }

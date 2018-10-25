92c2f5f6d2685725df4ea36ccdcc19d18789448d
Brian Henderson <brian.henderson1@marist.edu>
2018-10-24 20:23:52 -0400

    commiting to a new branch because im about to try something most likely dumb and want a reference

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,12 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+
 
 @Component({
     selector: 'app-edit',
     templateUrl: './edit.component.html',
 })
-export class EditComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit() {
+export class EditComponent implements OnInit {
+
+   orderForm: FormGroup;
+   items: FormArray;
+   formControlName="name"
+
+   constructor(private formBuilder: FormBuilder) { }
+
+   ngOnInit() {
+      this.orderForm = this.formBuilder.group({
+         customerName: '',
+         email: '',
+         items: this.formBuilder.array([this.createItem()])
+      });
+   }
+
+   createItem(): FormGroup {
+      return this.formBuilder.group({
+        name: '',
+        description: '',
+        price: ''
+      });
     }
+
+    addItem(): void {
+      this.items = this.orderForm.get('items') as FormArray;
+      this.items.push(this.createItem());
+    }
+
+    printItems(): void {
+      this.items = this.orderForm.get('items') as FormArray;
+      console.log(this.items);
+    }
+
+
+
 }

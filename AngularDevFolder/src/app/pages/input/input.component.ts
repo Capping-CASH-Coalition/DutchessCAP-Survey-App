a6e298504157801bc2d9f75c1a9afddd5261b18c
Brian Henderson <brian.henderson1@marist.edu>
2018-11-10 00:49:49 -0500

    starting dev on manual submit

--- AngularDevFolder/src/app/pages/input/input.component.ts
+++ AngularDevFolder/src/app/pages/input/input.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Globals } from '../../globals';
 
 @Component({
     selector: 'app-input',
     templateUrl: './input.component.html',
 })
 export class InputComponent implements OnInit {
-    constructor() { }
+    constructor( private globals: Globals) { }
 
     ngOnInit() {
     }
--- AngularDevFolder/src/app/pages/input/input.module.ts
+++ AngularDevFolder/src/app/pages/input/input.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import {InputComponent} from "./input.component";
-import {inputRoutes} from "./input.routes";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { InputComponent } from "./input.component";
+import { inputRoutes } from "./input.routes";
+import { Globals } from '../../globals';
 
 @NgModule({
-  imports: [
+   imports: [
       CommonModule,
       FormsModule,
       RouterModule.forChild(inputRoutes)
-  ],
-  declarations: [
-    InputComponent
-  ],
+   ],
+   declarations: [
+      InputComponent
+   ],
+   providers: [
+      Globals
+   ]
 })
 export class InputModule { }

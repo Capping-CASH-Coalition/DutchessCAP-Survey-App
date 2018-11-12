e0fb8cf9325c0873fa318ee2e99954c8ef58d000
Brian Henderson <brian.henderson1@marist.edu>
2018-11-12 12:27:40 -0500

    fixed merge issue

--- AngularDevFolder/src/app/app.module.ts
+++ AngularDevFolder/src/app/app.module.ts
@@ -5,30 +5,30 @@ import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import {Topnavbar} from "./components/topnavbar/topnavbar.component";
-import {Navigation} from "./components/navigation/navigation.component";
-import {RouterModule} from "@angular/router";
-import {appRoutes} from "./app.routes";
-import {HomeComponent} from "./pages/home/home.component";
+import { Topnavbar } from "./components/topnavbar/topnavbar.component";
+import { Navigation } from "./components/navigation/navigation.component";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./app.routes";
+import { HomeComponent } from "./pages/home/home.component";
 import { SurveyService } from './survey.service';
 import { KeysPipe } from './keys.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Navigation,
-    Topnavbar,
-    HomeComponent,
-    KeysPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    HttpClientModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  providers: [SurveyService, AppComponent],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      Navigation,
+      Topnavbar,
+      HomeComponent,
+      KeysPipe
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes)
+   ],
+   providers: [SurveyService, AppComponent],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }

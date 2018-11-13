41d2ade6274919639af69448e6a06a2f11d75bb2
Brian Henderson <brian.henderson1@marist.edu>
2018-11-13 01:07:38 -0500

    Chart now updates using data from DB for 1 question, data maps efficiently, need to make matrix data

--- AngularDevFolder/src/app/pages/graphs/graph.service.ts
+++ AngularDevFolder/src/app/pages/graphs/graph.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ChartColor } from 'chart.js';
 
 @Injectable()
 export class GraphService {
@@ -13,8 +14,37 @@ export class GraphService {
       {val: 'bubble', view: 'Bubble Hubble' },
    ]
 
+   private colors: string[] = [
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 99, 132, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255,1)',
+      'rgba(255, 159, 64, 1)'
+   ]
+   private chartOptions = {
+      responsive: false
+   }
+
+
    public getGraphTypes(): any[] {
       return this.graphTypes;
    }
+
+   public getColors(): string[] {
+      return this.colors;
+   }
+
+   public getOptions(): any {
+      return this.chartOptions;
+   }
+
+   public downloadChart(event, canvas) {
+      let anchor = event.target;
+      let can = document.getElementsByTagName(canvas)[0] as HTMLCanvasElement;
+      anchor.href = can.toDataURL("image/png");
+      anchor.download = "Graph.png";
+   }
+
 
 }
--- AngularDevFolder/src/app/pages/graphs/graphs.component.ts
+++ AngularDevFolder/src/app/pages/graphs/graphs.component.ts
@@ -21,7 +21,7 @@ export class GraphsComponent implements AfterViewInit, OnInit {
       private fb: FormBuilder,
    ){};
 
-   currentDatasetType: string;
+   currentDatasetType: string = 'single';
    chartForm: FormGroup;
    canvas: any;
    ctx: any;
@@ -34,13 +34,41 @@ export class GraphsComponent implements AfterViewInit, OnInit {
    ngAfterViewInit() {
      this.canvas = document.getElementById('graphCanvas');
      this.ctx = this.canvas.getContext('2d');  
-     // this.initChart();
+     this.updateChart();
    };
+
+   mapSingleData(): Map<string, number> {
+      let map = new Map();
+      let survey = this.globals.surveys[this.chartForm.controls.surveyId.value];
+      survey.questions.forEach(question => {
+         if (question.question_id == this.chartForm.controls.questionId.value) {
+            question.responses.forEach(r => {
+               if ( map.has(r.response_text) ) {
+                  let count = map.get(r.response_text);
+                  count++;
+                  map.set(r.response_text, count);
+               }
+               else {
+                  map.set(r.response_text, 1);
+               }
+            });
+         }
+      });
+      return map;
+   }
+
+   updateChart(): void {
+      let map: Map<string, number>;
+      if ( this.currentDatasetType === 'single') {
+         map = this.mapSingleData();
+      }
+      this.destroyChart();
+      this.buildChart(this.createSingleChart(map));
+   }
 
    updateMultipleDataSetForm(val){
       this.currentDatasetType = val;
    }
-
 
    // will need to create methods to get surveyId, questionId, subQuestionId,
    initChartForm() {
@@ -52,7 +80,6 @@ export class GraphsComponent implements AfterViewInit, OnInit {
         // subQuestionOptions: this.getSubQuestionOptions(this.chartForm.controls.subQuestionId.value)
       });
    }  
-
 
    getSubQuestionOptions(subQuestionId): FormArray {
       let ops = new FormArray([]);
@@ -68,151 +95,35 @@ export class GraphsComponent implements AfterViewInit, OnInit {
       return ops;
    }
 
-   /*
-
-   patchFormOptions(options) {
-      let ops = new FormArray([]);
-      options.forEach(o => {
-         ops.push(this._fb.group({
-            option: new FormControl(o.option_text)
-         }));
-      });
-      return ops;
-   }
-
-   initQuestion() {
-      return this._fb.group({
-         questionText: new FormControl(''),
-         questionType: new FormControl(''),
-         questionOptions: this._fb.array([
-            this.initOption(),
-         ])
-      });
-   }
-
-   initOption() {
-      return this._fb.group({
-         option: new FormControl('')
-      })
-   }
-
-
-   // Data Filters/Form Group
-  // chartFiltersForm: FormGroup;
-/*   chartDataFilters: string[] = [];
-   chartTypes: string[] = [];
-   
-   chartTypesObj: ChartType[] = [];
-
-   //Canvas
-   canvas: any;
-   ctx: any;
-
-   //Chart Object
-   chart: Chart = null;
-
-   //Chart Data (Initialize it here with a length of 1 so don't get undefined error)
-   chartDataArr: Array<GraphData> = new Array(1);
-
-   //Chart Globals
-   currChartType: string;
-   currDataLabels: string[];
-   currDataSetData: number[];
-   currDataFilterTopLevel: string;
-   currDataFilterSubLevel: string;
-   
-   chartOptions = {
-      responsive: false
-   }
-
-
-   updateTopFilter(filter: string) {
-      if (this.currDataFilterTopLevel != filter) {
-         this.currDataFilterTopLevel = filter;
-         this.updateCurrCharDataLabels(filter);
-         this.generateChart();
-      }
-   }
-
-   updateSubFilter(filter: string) {
-      if (this.currDataFilterSubLevel != filter) {
-         this.currDataFilterSubLevel = filter;
-      }
-   }
-
-   updateChartType(chartType: string) {
-      if (this.currChartType != chartType) {
-         this.currChartType = this.getChartJSChartType(chartType);
-         this.generateChart();
-      }
-   }
-
-   private updateCurrCharDataLabels(filter: string): void {
-      let gd = this.getGraphDataFromDataArray(filter);
-      this.currDataSetData = gd.data;
-      this.currDataLabels = gd.labels;
-   }
-
-   private generateChart(): void {
-      this.chart.destroy();
-      this.chart = this.getCurrChartGlobals();
-      this.chart.update();
-   }
-
-   private initChart(): void {
-      this.chart = this.getCurrChartGlobals();
-      this.chart.update();
-   }
-
-
-
-   private initChartGlobals(gd: GraphData): void {
-      this.currChartType = 'pie';
-      this.currDataLabels = gd.labels;
-      this.currDataSetData = gd.data;
-   }
-
-   private getCurrChartGlobals(): Chart {
-      let c = new Chart(this.ctx, {
-         type: this.currChartType,
+   createSingleChart(map: Map<string, number>): Chart {
+      return new Chart(this.ctx, {
+         type: this.chartForm.controls.chartType.value,
          data: {
-            labels: this.currDataLabels,
+            labels: Array.from(map.keys()),
             datasets: [{
-                label: 'Total Distribution',
-                data: this.currDataSetData,
-                backgroundColor: [
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255,1)',
-                    'rgba(255, 159, 64, 1)'
-                ]
+               label: 'Total Distribution of Question Answers',
+               data: Array.from(map.values()),
+               backgroundColor: this.graphService.getColors()
             }]
-        },
-         options: this.chartOptions
-      })
-      return c;
+         },
+         options: this.graphService.getOptions()
+      });
    }
 
-   private addToDataArray(key:string, labels:Array<string>, data:Array<number>): void {
-      let gd = new GraphData(key, labels, data);
-      this.chartDataArr.push(gd);
+   private buildChart(chartData: Chart): void {
+      this.chart = chartData;
+      this.chart.update();
    }
 
-   private getGraphDataFromDataArray(key: string): GraphData {
-      for (let i = 0; i < this.chartDataArr.length; i++) {
-         if (this.chartDataArr[i].key === key) {
-            return this.chartDataArr[i];
-         }
+   private destroyChart(): void {
+      if (this.chart != null) {
+         this.chart.destroy();
       }
    }
 
    download(event) {
-    var anchor = event.target;
-    var can =document.getElementsByTagName('canvas')[0] as HTMLCanvasElement;
-    anchor.href = can.toDataURL("image/png");
-    anchor.download = "graph.png";
-}
-*/   
+      this.graphService.downloadChart(event, 'canvas');
+   }
+   
+
 }

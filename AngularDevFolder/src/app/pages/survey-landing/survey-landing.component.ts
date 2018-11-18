1ce6e4eb124f49ab3f5a17aadb83d557739d7bbd
Stephen Stone <sstone2423@gmail.com>
2018-11-18 14:06:57 -0500

    First attempt at linking surveylanding to survey

--- AngularDevFolder/src/app/pages/survey-landing/survey-landing.component.ts
+++ AngularDevFolder/src/app/pages/survey-landing/survey-landing.component.ts
@@ -2,6 +2,7 @@ import { Globals } from '../../globals';
 import { Component} from '@angular/core';
 import { stringify } from 'querystring';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-surveyLanding',
@@ -11,16 +12,17 @@ import { NgForm } from '@angular/forms';
 
 export class SurveyLandingComponent {
 
-constructor(private globals: Globals) { }
+constructor(private globals: Globals,
+            public router: Router) { }
 
   test = [];
   selectedVersion: number;
 
-  /*onStart(start: NgForm){
-    this.test.push(start.value);
-    console.log(this.test[0]);
+  onStart(){
+    this.router.navigate(['survey']);
+    //this.test.push(start.value);
+    //console.log(this.test[0]);
   }
-*/
   surveySelect($event, value){
   this.selectedVersion = value;
   console.log(this.selectedVersion);

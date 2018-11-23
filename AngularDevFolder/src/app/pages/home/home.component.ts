507756912dd9328010df76a81a96c390864c038a
Zachary Ropes <zachary.ropes1@marist.edu>
2018-11-23 13:29:31 -0500

    Added Date Graph

--- AngularDevFolder/src/app/pages/home/home.component.ts
+++ AngularDevFolder/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { OnInit, Component } from "@angular/core";
+import { OnInit, Component, AfterViewInit } from "@angular/core";
 import { Globals } from "../../globals";
+import { GraphService } from '../../services/graph.service';
 
 @Component({
    selector: 'home',
@@ -8,10 +9,38 @@ import { Globals } from "../../globals";
 })
 export class HomeComponent implements OnInit {
    constructor(
-      public globals: Globals
+      public globals: Globals,
+      public graphService: GraphService
    ) { };
 
    ngOnInit() {
+   }
+
+   canvas: any;
+   // canvas context
+   ctx: any;
+   // chart object
+   chart: Chart = null;
+
+   ngAfterViewInit() {
+      this.canvas = document.getElementById('graphCanvas');
+      this.ctx = this.canvas.getContext('2d');
+      this.updateChart();
+   };
+
+   
+
+
+   updateChart(): void {
+      this.destroyChart();
+      let c: Chart = this.graphService.createDateChart(this.ctx, "line", this.DateGraphData())
+      this.buildChart(c)
+}
+
+   private destroyChart(): void {
+      if (this.chart != null) {
+         this.chart.destroy();
+      }
    }
 
    // go through the surveys and get the info for the survey details card
@@ -33,4 +62,108 @@ export class HomeComponent implements OnInit {
       return surveyDetails;
    }
 
+   private buildChart(chartData: Chart): void {
+      this.chart = chartData;
+      this.chart.update();
+   }
+
+   
+   
+   mapDateData(val): Map<string, number> {
+      let map = new Map();
+      let survey = this.globals.surveys[val];
+      let qid = survey.questions[0].question_id;
+      survey.questions.forEach(question => {
+         if (question.question_id == qid) {
+            question.responses.forEach(r => {
+               let testdate = new Date(r.date_taken)
+               if (testdate >= this.getDateYearAgo()){
+                  if (map.has(r.date_taken)) {
+                     let count = map.get(r.date_taken);
+                     // increment count by 1
+                     count += 1;
+                     map.set(r.date_taken, count);
+                  }
+                  else {
+                     map.set(r.date_taken, 1);
+                  }
+               }
+            });
+
+         }
+      });
+      return map;
+   }
+
+
+
+   mapDateDataSets(): any[] {
+      let datasets: any[] = new Array();
+      // go through the checkboxes that are selected
+         // dsMap contains all the top question options labels with values 0
+         for(let v = 0; v < this.globals.surveys.length; v++){
+            // push the dataset values
+            datasets.push({
+               label: this.globals.surveys[v].survey_name,
+               data: this.mapDataLast(v),
+               borderColor: this.graphService.getColorByIndex(v),
+               fill: false
+            })
+         };
+      return datasets;
+   }
+
+   mapDataLast(val): any[] {
+      let data: any[] = new Array();
+      let a = Array.from(this.mapDateData(val).keys());
+      let b = Array.from(this.mapDateData(val).values());
+
+      let survey = this.globals.surveys[val];
+
+      // Push the values with the labels to the datasets
+      for(let r=0; r <= survey.questions[0].responses.length; r++){
+
+         data.push({
+            x: a[r],
+            y: b[r]
+         })
+
+      }
+      return data
+   }
+
+
+   // pulls together the above functions to set the labels and datasets for the matrix graph
+   DateGraphData(): any {
+      return {
+         datasets: this.mapDateDataSets()
+      }
+   }
+
+   private getDateYearAgo(): any {
+      let today = new Date();
+      let d = today.getDate();
+      let m = today.getMonth() + 1;
+      let yyyy = today.getFullYear() - 1;
+      let mm: string;
+      let dd: string;
+
+      if(d < 10) {
+         dd = '0' + d;
+      }
+      else{
+         dd = '' + d;
+      }
+      if(m < 10) {
+         mm = '0' + m;
+      }
+      else{
+         mm = '' + m;
+      }
+
+      let today1 = new Date(yyyy + '-' + mm + '-' + dd);
+
+      return today1;
+   }
+   
 }
